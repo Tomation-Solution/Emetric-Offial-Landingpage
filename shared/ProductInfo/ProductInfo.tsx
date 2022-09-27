6a234a28bb7a0bcbebb6f4d49656038a90f4c443
markothedev <markothedevmail@gmail.com>
2022-09-27 19:28:37 +0100

    added our product to the landing page

--- shared/ProductInfo/ProductInfo.tsx
+++ shared/ProductInfo/ProductInfo.tsx
@@ -1,14 +1,35 @@
+import Button from '../Button/Button'
+import { ProductInfoContentContainer, ProductInfoImageContainer, ProductInfoMainContainer ,ProductInfoCover} from './ProductInfo.style'
+import rounded_transparent from '../../assets/rounded-transparent.png'
 
 
-
-
-
-const ProductInfo = ():React.ReactElement=>{
+type Prop ={
+  heading:string;
+  body:string;
+  image:string
+  variant?:'var1' | 'var2'
+}
+const ProductInfo = ({heading,body,image,variant='var1'}:Prop):React.ReactElement=>{
 
   return(
-    <div>
-      <h1>Product info</h1> 
-    </div>  
+    <ProductInfoCover variant={variant}>
+      <img className='rounded_transparent_svg' src={rounded_transparent.src} alt="" />
+      <ProductInfoMainContainer variant={variant}>
+        <ProductInfoImageContainer>
+          <img src={image} alt="" />
+        </ProductInfoImageContainer>
+        <ProductInfoContentContainer>
+          <h2>{heading}</h2>
+          <p>
+            {body}
+          </p>
+          <br />
+          <Button>
+          Get Started
+          </Button>
+        </ProductInfoContentContainer>
+      </ProductInfoMainContainer>  
+    </ProductInfoCover>
   )
 }
 
