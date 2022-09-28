c582f79de2de2dbe27f3e37b54007dfd1be9a205
markothedev <markothedevmail@gmail.com>
2022-09-28 14:33:03 +0100

    added our team page

---
+++ pages/our_team.tsx
@@ -0,0 +1,65 @@
+import DetailLayout from '../layout/DetailLayout/DetailLayout'
+import { Team, TeamContainer, TeamImgContainer, TeamSocials } from '../styles/our_team.style'
+import OzeImage from '../assets/oze.jpeg'
+import { BsLinkedin} from 'react-icons/bs'
+
+
+
+const OurTeam = ():React.ReactElement=>{
+
+
+
+  return (
+    <DetailLayout
+      heading='Our Team'
+      body='We have a team of experienced HR consultants with substansive years of experience'
+    >
+
+      <TeamContainer>
+        <Team>
+          <TeamImgContainer>
+            <img src={OzeImage.src} alt="" />
+          </TeamImgContainer>
+
+          <h2>Mr. Ozed Osawe</h2>
+          <p>Director of Business Operations <br />
+             (EMEA)</p>
+          <TeamSocials>
+            <BsLinkedin/>
+          </TeamSocials>
+        </Team>
+        <br />
+        <br />
+        <Team>
+          <TeamImgContainer>
+            <img src={OzeImage.src} alt="" />
+          </TeamImgContainer>
+
+          <h2>Debo Adebayo</h2>
+          <p>
+          Founder/CEO 
+          </p>
+          <TeamSocials>
+            <BsLinkedin/>
+          </TeamSocials>
+        </Team>
+
+        <br />
+        <br />
+        <Team>
+          <TeamImgContainer>
+            <img src={OzeImage.src} alt="" />
+          </TeamImgContainer>
+
+          <h2>Robert P. Ebers</h2>
+          <p>Chief Executive Officer</p>
+          <TeamSocials>
+            <BsLinkedin/>
+          </TeamSocials>
+        </Team>
+      </TeamContainer>
+    </DetailLayout>
+  )
+}
+
+export default OurTeam
