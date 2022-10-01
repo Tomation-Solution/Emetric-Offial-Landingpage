30962d25522c3bfb928901ea266a77265d44723b
markothedev <markothedevmail@gmail.com>
2022-10-01 21:12:54 +0100

    added localhost to the cors settings

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,17 +1,31 @@
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import BlogLayout from '../../layout/BlogLayout/BlogLayout';
+import api from '../../services/api';
 import ExploreBlogTopics from '../../shared/ExploreBlogTopics/ExploreBlogTopics';
 import PostPreview from '../../shared/PostPreview/PostPreview';
 
 
 
 const BlogIndex:NextPage = ()=>{
+  const [loading,setLoading] = useState(false)
+  const getBlogs = async()=>{
+    setLoading(true)
+    console.log()
+    const resp = await api.get('blog/blog-view/')
+    setLoading(false)
 
+    console.log(resp)
+      
+  }
 
+  useEffect(()=>{
+    getBlogs()
+  },[])
   return (
     <BlogLayout>
       <br />
-
+      {loading &&<h1>Loading..</h1>}
       <PostPreview/>
       <PostPreview variant='var2'/>
 
---
+++ services/api.tsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+
+
+const api = axios.create({
+  baseURL: 'https://emetric-landingpage-backend.herokuapp.com/',
+ 
+});
+
+
+
+
+export default api
