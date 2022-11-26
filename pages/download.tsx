6f78677803a1f92796b72f6a77f89162230dce8d
markothedev <markothedevmail@gmail.com>
2022-11-26 09:24:16 +0100

    updated qrcode download link

--- pages/download.tsx
+++ pages/download.tsx
@@ -4,7 +4,7 @@ const Download =  ()=>{
 
   useEffect(()=>{
     if(window !=undefined){
-      window.location.href='https://drive.google.com/file/d/1kmRtwUPBdJEgjVU8LDAxaxtV__6M7wQC/view'
+      window.location.href='https://drive.google.com/file/d/1f9YC4sNgZjzm_FRDmmEsyQgnneceTDN4/view?usp=share_link'
     }
   },[])
 
